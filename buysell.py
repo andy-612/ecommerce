5a3c6c7da42eec6e0a4ba5fd611af6ed0b1e0135
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2018-01-12 08:44:15 -0500

    view user

--- buysell.py
+++ buysell.py
@@ -1,6 +1,6 @@
 #for testing usernames, users enter "George"
 #for selling, users enter "Adidas"
-#users should be added to list
+#users should be added to list ---
 #[{'name': 'Andy', 'selling': ['nike8', 'Addidas9']}, {'name': 'George', 'selling': ['Adidas']}]
 
 #first we have a list of users where each useer is a dictionary
@@ -11,7 +11,14 @@ def signup():
     user["name"] = input("What is your name?")
     user["selling"].append(input("What are you selling?"))
     users.append(user)
-
+def view_profile(pname):
+    for i in users:
+        if i["name"]==pname:
+            print(i["name"], "is currently selling ")
+            for j in i["selling"]:
+                print(j)
 signup()
 print(users)
+profile=input("which user do you want to view")
+view_profile(profile)
                          
